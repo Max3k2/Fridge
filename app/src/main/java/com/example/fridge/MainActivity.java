23ba677f79adfe6b668cad52728e7cd23f860225
Max3k2 <bmaks632@gmail.com>
2023-02-11 22:02:54 +0100

    Commit

--- app/src/main/java/com/example/fridge/MainActivity.java
+++ app/src/main/java/com/example/fridge/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fridge;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    public int splashDuration = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+      new Handler().postDelayed(() -> {
+          Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+          startActivity(intent);
+      }, splashDuration);
     }
 }
